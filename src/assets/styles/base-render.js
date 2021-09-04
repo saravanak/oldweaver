c8b74d8991e243e2944590d4450f054a4c6a4da2
Saravana <vsk62.gate@gmail.com>
2021-09-04 11:36:30 +0530

    Add milligram and integrate the resume page

--- src/assets/styles/base-render.js
+++ src/assets/styles/base-render.js
@@ -1,26 +1,8 @@
-// This file handles the CSS build.
-// It will run Sass and compile all styles defined in the main entry file.
-
-// main entry point name
-const ENTRY_FILE_NAME = 'main.scss'
-
-const path = require('path')
 const sass = require('node-sass')
-const CleanCSS = require('clean-css')
-const cssesc = require('cssesc')
 const isProd = process.env.ELEVENTY_ENV === 'production'
 
-module.exports = class {
-    async data() {
-        const entryPath = path.join(__dirname, `/${ENTRY_FILE_NAME}`)
-        return {
-            permalink: `/assets/styles/main.css`,
-            eleventyExcludeFromCollections: true,
-            entryPath
-        }
-    }
-
-    // Compile Sass to CSS,
+class BaseRender{
+// Compile Sass to CSS,
     // Embed Source Map in Development
     async compile(config) {
         return new Promise((resolve, reject) => {
@@ -117,3 +99,4 @@ module.exports = class {
         }
     }
 }
+module.exports = BaseRender;
---
+++ src/assets/styles/main.css.11ty.js
@@ -0,0 +1,23 @@
+// This file handles the CSS build.
+// It will run Sass and compile all styles defined in the main entry file.
+
+// main entry point name
+const ENTRY_FILE_NAME = 'main.scss'
+
+const path = require('path')
+const CleanCSS = require('clean-css')
+const cssesc = require('cssesc')
+const BaseRender = require('./base-render');
+
+class MainRender extends BaseRender {
+        async data() {
+            const entryPath = path.join(__dirname, `/${ENTRY_FILE_NAME}`)
+            return {
+                permalink: `/assets/styles/main.css`,
+                eleventyExcludeFromCollections: true,
+                entryPath
+            }
+        }
+}
+
+module.exports = MainRender
